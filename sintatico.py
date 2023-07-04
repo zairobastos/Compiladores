aa80205d9f5b931f8bddda81f27a1f4fcaea5fe3
Carlos Eduardo <cerpcaca2013@gmail.com>
2023-07-03 23:00:31 -0300

    Ajustes pontuais e build dos blocos de negacao, expr_arit e expr_logica

--- sintatico.py
+++ sintatico.py
@@ -1,5 +1,3 @@
-from token_1 import Token
-
 class Sintatico:
 
     def __init__(self, tokens):
@@ -13,13 +11,13 @@ class Sintatico:
             self.token_atual = self.tokens[self.indice]
             self.indice += 1
         else:
-            self.token_atual = None
+            self.token_atual = ('FIM DO CÓDIGO', 'EOF', self.tokens[self.indice - 1][2])
 
     def get_two_ahead(self):
-        if(self.indice < len(self.tokens)):
+        if(self.indice + 1 < len(self.tokens)):
             return self.tokens[self.indice + 1]
         else:
-            return None;
+            return ('FIM DO CÓDIGO', 'EOF', self.tokens[self.indice - 1][2])
 
     def erro(self, esperado):
         if self.token_atual is None:
@@ -201,7 +199,7 @@ class Sintatico:
             self.proximo_token()
             if(linha_codigo != self.token_atual[2]):
                 self.erro_linha('IDENTIFICADOR, NUMERO INTEIRO, DECIMAL, BOOLEANO OU STRING', 1)
-            
+
             self.expressao_simples()
 
     def condicional(self):
@@ -214,15 +212,6 @@ class Sintatico:
         if self.token_atual[0] != 'SÍMBOLOS' or self.token_atual[1] != '(':
             self.erro('SÍMBOLO "("')
 
-        self.expressao()
-
-        self.proximo_token()
-        if(linha_codigo != self.token_atual[2]):
-            self.erro_linha('SÍMBOLO DE COMPARAÇÃO OU OPERADOR RELACIONAL', 1) 
-
-        if self.token_atual[0] != 'SÍMBOLO DE COMPARAÇÃO' and self.token_atual[0] != 'OPERADOR RELACIONAL':
-            self.erro('SÍMBOLO DE COMPARAÇÃO OU OPERADOR RELACIONAL')
-        
         self.expressao()
 
         self.proximo_token()
@@ -243,8 +232,11 @@ class Sintatico:
         if(linha_codigo == self.token_atual[2]):
             self.erro_linha(f'{self.token_atual[0]} {self.token_atual[1]} na linha {self.token_atual[2]}', 0)
         
-        self.declaracoes()
-        self.comandos()
+        if self.token_atual[0] == 'SÍMBOLOS' and self.token_atual[1] == '}':
+            return
+        else:
+            self.declaracoes()
+            self.comandos()
 
         have_else = (self.get_two_ahead()[1] == 'else' and self.get_two_ahead()[0] == 'PALAVRA RESERVADA')
 
@@ -263,58 +255,106 @@ class Sintatico:
             if(linha_codigo == self.token_atual[2]):
                 self.erro_linha(f'{self.token_atual[0]} {self.token_atual[1]} na linha {self.token_atual[2]}', 0)
             
-            self.declaracoes()
-            self.comandos()
+            if self.token_atual[0] == 'SÍMBOLOS' and self.token_atual[1] == '}':
+                return
+            else:
+                self.declaracoes()
+                self.comandos()
 
             self.proximo_token()
             if self.token_atual[0] != 'SÍMBOLOS' or self.token_atual[1] != '}':
                 self.erro('SÍMBOLO "}"')
 
-
     def expressao(self):
         linha_codigo = self.token_atual[2]
+        decisao = self.get_two_ahead()
 
         self.proximo_token()
+        
         if(linha_codigo != self.token_atual[2]):
             self.erro_linha('OPERADOR LÓGICO "!", IDENTIFICADOR OU VALOR DE VARIAVEL', 1)
         
-        if self.token_atual[0] == 'OPERADOR LÓGICO' and self.token_atual[1] == '!': #NEGAÇÃO
+        if decisao[0] == 'IDENTIFICADOR' or decisao[0] == 'NUMERO INTEIRO' or decisao[0] == 'NUMERO DECIMAL' or decisao[0] == 'BOOLEANO' or decisao[0] == 'STRING': #NEGAÇÃO
             self.negacao()
 
-        # EXPRESSÕES (LÓGICA OU ARITMETICA)
-        elif self.token_atual[0] == 'IDENTIFICADOR' or self.token_atual[0] == 'NUMERO INTEIRO' or self.token_atual[0] == 'NUMERO DECIMAL' or self.token_atual[0] == 'BOOLEANO' or self.token_atual[0] == 'STRING':
-            self.proximo_token()
+        # EXPRESSÕES
+        elif decisao[0] == 'OPERADOR ARITMÉTICO':
+            self.expressao_arit()
 
-            if(linha_codigo != self.token_atual[2]):
-                self.erro_linha('OPERADOR LÓGICO OU ARITMÉTICO', 1)
+        elif decisao[0] == 'OPERADOR RELACIONAL' or decisao[0] == 'SÍMBOLO DE COMPARAÇÃO' or decisao[0] == 'OPERADOR LÓGICO':
+            self.expressao_logica()
 
-            if self.token_atual[0] == 'OPERADOR LÓGICO':
-                self.expressao_logica()
-            elif self.token_atual[0] == 'OPERADOR ARITMÉTICO':
-                self.expressao_arit()
-            else:
-                self.erro('OPERADOR LÓGICO OU ARITMÉTICO', 1)
         else:
             self.erro('OPERADOR LÓGICO "!", IDENTIFICADOR, NUMERO INTEIRO, DECIMAL, BOOLEANO OU STRING', 1)
 
     def negacao(self):
-        self.expressao_logica()
-
-    def expressao_logica(self):
         linha_codigo = self.token_atual[2]
-        self.proximo_token()
-
-    def expressao_arit(self):
-        linha_codigo = self.token_atual[2]
+        if self.token_atual[0] != 'OPERADOR LÓGICO' or self.token_atual[1] != '!':
+            self.erro('OPERADOR LÓGICO "!"')
 
         self.proximo_token()
-
         if(linha_codigo == self.token_atual[2]):
             self.expressao_simples()
         else:
             self.erro_linha('IDENTIFICADOR, NUMERO INTEIRO, DECIMAL, BOOLEANO OU STRING', 1)
 
+    def expressao_logica(self):
+        linha_codigo = self.token_atual[2]
+        self.expressao_simples()
+
+        self.proximo_token()
+        if(linha_codigo != self.token_atual[2]):
+            self.erro_linha('OPERADOR LÓGICO, OPERADOR RELACIONAL OU SÍMBOLO DE COMPARAÇÃO', 1)
+        
+        if self.token_atual[0] != 'OPERADOR LÓGICO' and self.token_atual[0] != 'SÍMBOLO DE COMPARAÇÃO' and self.token_atual[0] != 'OPERADOR RELACIONAL':
+            self.erro('OPERADOR LÓGICO, OPERADOR RELACIONAL OU SÍMBOLO DE COMPARAÇÃO')
+        
+        have_operator = self.get_two_ahead()
+        
+        self.proximo_token()
+        if(linha_codigo != self.token_atual[2]):
+            self.erro_linha('IDENTIFICADOR, NUMERO INTEIRO, DECIMAL, BOOLEANO OU STRING', 1)
+
+        self.expressao_simples()
+        if(have_operator[0] == 'OPERADOR LÓGICO' or have_operator[0] == 'SÍMBOLO DE COMPARAÇÃO' or have_operator[0] == 'OPERADOR RELACIONAL'):
+            self.expressao_logica()
+        elif have_operator[0] == 'OPERADOR ARITMÉTICO':
+            self.expressao_arit()
+        elif have_operator == None or have_operator[2] != linha_codigo or have_operator[1] == ')':
+            pass 
+        else:  
+            self.erro_linha('OPERADOR LÓGICO, OPERADOR RELACIONAL, SÍMBOLO DE COMPARAÇÃO OU OPERADOR ARITMÉTICO', 1)
+
+    def expressao_arit(self):
+        linha_codigo = self.token_atual[2]
+
+        self.expressao_simples()
+
+        self.proximo_token()
+        if(linha_codigo != self.token_atual[2]):
+            self.erro_linha('OPERADOR ARITMÉTICO', 1)
+
+        if self.token_atual[0] != 'OPERADOR ARITMÉTICO':
+            self.erro('OPERADOR ARITMÉTICO')
+        
+        have_operator = self.get_two_ahead()
+
+        self.proximo_token()
+        if(linha_codigo != self.token_atual[2]):
+            self.erro_linha('IDENTIFICADOR, NUMERO INTEIRO, DECIMAL, BOOLEANO OU STRING', 1)
+
+        self.expressao_simples()
+
+        if(have_operator[0] == 'OPERADOR LÓGICO' or have_operator[0] == 'SÍMBOLO DE COMPARAÇÃO' or have_operator[0] == 'OPERADOR RELACIONAL'):
+            self.expressao_logica()
+        elif have_operator[0] == 'OPERADOR ARITMÉTICO':
+            self.expressao_arit()
+        elif have_operator == None or have_operator[2] != linha_codigo  or have_operator[1] == ')':
+            pass
+        else:  
+            self.erro_linha('OPERADOR LÓGICO, OPERADOR RELACIONAL, SÍMBOLO DE COMPARAÇÃO OU OPERADOR ARITMÉTICO', 1)
+
     def expressao_simples(self):
 
         if self.token_atual[0] != 'IDENTIFICADOR' and self.token_atual[0] != 'NUMERO INTEIRO' and self.token_atual[0] != 'NUMERO DECIMAL' and self.token_atual[0] != 'BOOLEANO' and self.token_atual[0] != 'STRING':
-            self.erro('IDENTIFICADOR, NUMERO INTEIRO, DECIMAL, BOOLEANO OU STRING', 1)        
+            self.erro('IDENTIFICADOR, NUMERO INTEIRO, DECIMAL, BOOLEANO OU STRING')        
--- token_1.py
+++
@@ -1,6 +0,0 @@
-import re
-class Token:
-    def __init__(self,tipo,valor,linha):
-        self.tipo: tipo
-        self.valor: valor
-        self.linha: linha
